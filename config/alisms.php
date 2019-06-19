9a54e12e85c3d6973bf1601d70b203ad93aa9c3a
wyh <rmzf@qq.com>
2019-06-19 20:11:34 +0800

    complete test

--- config/alisms.php
+++ config/alisms.php
@@ -10,6 +10,6 @@ return [
     'region_id'         => env('ALI_SMS_REGION_ID', 'cn-hangzhou'), // regionid
     'access_key'        => env('ALI_SMS_ACCESS_KEY_ID', '111111'), // accessKey
     'access_secret'     => env('ALI_SMS_ACCESS_SECRET', 'xxxxxxxxx'), // accessSecret
-    'sign_name'         => env('ALI_SMS_SIGN_NAME', 'pc'), // 签名
+    'sign_name'         => env('ALI_SMS_SIGN_NAME', '找PC'), // 签名
     'version'           => env('ALI_SMS_VERSION', '2017-05-25'), // 签名
 ];
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,22 @@
 
 class TestCase extends Orchestra\Testbench\TestCase
 {
+
+    protected function getEnvironmentSetUp($app)
+    {
+        // make sure, our .env file is loaded
+        $app->useEnvironmentPath(__DIR__.'/..');
+        $app->bootstrapWith([\Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables::class]);
+        $app['config']->set('alisms',[
+            'region_id'         => env('ALI_SMS_REGION_ID', 'cn-hangzhou'), // regionid
+            'access_key'        => env('ALI_SMS_ACCESS_KEY_ID', '111111'), // accessKey
+            'access_secret'     => env('ALI_SMS_ACCESS_SECRET', 'xxxxxxxxx'), // accessSecret
+            'sign_name'         => env('ALI_SMS_SIGN_NAME', 'pc'), // 签名
+            'version'           => env('ALI_SMS_VERSION', '2017-05-25'), // 签名
+        ]);
+        parent::getEnvironmentSetUp($app);
+    }
+
     protected function getPackageProviders($app)
     {
         return ['Wuliaowyh\AliSms\AliSmsServiceProvider'];
@@ -25,6 +41,6 @@ class TestCase extends Orchestra\Testbench\TestCase
      *  @expectException        Exception
      */
     public function send(){
-        $this->assertIsBool(\Wuliaowyh\AliSms\Facades\AliSms::send('xxxxxxxxx', 'SMS_xxxx', ['code'=>'xxxx']));
+        $this->assertIsBool(\Wuliaowyh\AliSms\Facades\AliSms::send('18616257971', 'SMS_168305466', ['code'=>'xxxx', 'order'=>'xxxxx']));
     }
 }
